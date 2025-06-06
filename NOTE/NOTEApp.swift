74d5437fe0b4bac0fe5a65489c89a039db274ba8
rahul2025 <rs3@wgu.edu>
2025-06-06 16:26:26 +0900

    Initial Commit

---
+++ NOTE/NOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOTEApp.swift
+//  NOTE
+//
+//  Created by 渡海 on 2025/06/06.
+//
+
+import SwiftUI
+
+@main
+struct NOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
